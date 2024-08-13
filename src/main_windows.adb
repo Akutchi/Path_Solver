a4b0834bc62d14d8f1014ff0b834aa991976012b
Akutchi <kaitlyn.buffler@tutamail.com>
2024-08-13 12:01:30 +0200

    program now terminates

--- src/main_windows.adb
+++ src/main_windows.adb
@@ -1,14 +1,27 @@
-with Gtk.Window;
-with Gtk.Enums;
+with Gtk.Enums;    use Gtk.Enums;
+with Gtk.Handlers; use Gtk.Handlers;
+with Gtk.Widget;   use Gtk.Widget;
+with Gtk.Main;     use Gtk.Main;
 
 package body Main_Windows is
 
-   procedure Initialize (Win : access Main_Window_Record'Class) is
+   package Window_Cb is new Callback (Gtk_Widget_Record);
 
+   procedure Exit_Main (Object : access Gtk_Widget_Record'Class) is
+   begin
+      Destroy (Object);
+      Main_Quit;
+
+   end Exit_Main;
+
+   procedure Initialize (Win : access Main_Window_Record'Class) is
    begin
 
-      Gtk.Window.Initialize (Win, Gtk.Enums.Window_Toplevel);
+      Gtk.Window.Initialize (Win, Window_Toplevel);
       Set_Default_Size (Win, 800, 600);
+
+      Window_Cb.Connect
+        (Win, "destroy", Window_Cb.To_Marshaller (Exit_Main'Access));
 
    end Initialize;
 
